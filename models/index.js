7d682fb28b41a93456f9a98e36c9f8e161f668fd
The Duz <tamara.dusenbury@gmail.com>
2023-05-02 17:25:08 -0700

    create relationships and define columns in models files

---
+++ models/index.js
@@ -0,0 +1,34 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: ProductTag
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_id',
+  through: ProductTag
+  
+})
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
